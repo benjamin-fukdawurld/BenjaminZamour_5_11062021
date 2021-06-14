d4022e28cd72bcc9c2743129549a69f2b8704c80
benjamin <benjamin.zamour@gmail.com>
2021-06-14 22:34:29 +0200

    Add TeddyGenerators scripts

---
+++ orinoco/src/scripts/TeddyCartItemGenerator.js
@@ -0,0 +1,22 @@
+import AbstractTeddyGenerator from "./AbstractTeddyGenerator.js";
+
+export default class TeddyCartItemGenerator extends AbstractTeddyGenerator {
+    constructor() {
+        super();
+    }
+
+    generate({ teddy, rootElm, color, count }) {
+        this.hydrate({ teddy, rootElm, className: "cart-item" });
+        this.componentProxy.count.value = count;
+        this.componentProxy["total-price"].textContent = `${((count * teddy.price) / 100).toFixed(
+            2
+        )}€`;
+        this.hydrate({ teddy, rootElm, className: "cart-item__info" });
+        this.initImageElm();
+        this.initNameElm();
+        this.initPriceElm();
+        this.componentProxy.color.textContent = color;
+
+        return rootElm;
+    }
+}
