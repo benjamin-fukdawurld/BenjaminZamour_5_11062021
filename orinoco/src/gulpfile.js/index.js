b74f7e8d78a7dd9d4a4c7514e47ca20162b4447a
benjamin <benjamin.zamour@gmail.com>
2021-06-17 22:25:28 +0200

    Add tailwind and purgecss to gulp workflow

--- orinoco/src/gulpfile.js/index.js
+++ orinoco/src/gulpfile.js/index.js
@@ -2,25 +2,26 @@ require("dotenv").config();
 
 process.env.outputDir = process.env.outputDir ?? "./dist";
 
-const { watch, parallel } = require("gulp");
+const { watch, parallel, series } = require("gulp");
 
-const { style } = require("./style");
+const { style, cleancss } = require("./style");
 const { js } = require("./js");
 const { html } = require("./html");
 const { images } = require("./images");
 
 exports.style = style;
+exports.cleancss = cleancss;
 exports.html = html;
 exports.js = js;
 exports.images = images;
 
 exports.watch = function () {
-    watch("./scss/**/*.scss", { ignoreInitial: false }, style);
-    watch("./views/**/*", { ignoreInitial: false }, html);
+    watch("./scss/**/*.scss", { ignoreInitial: false }, series(style, cleancss));
+    watch("./views/**/*", { ignoreInitial: false }, series(html, cleancss));
     watch("./resources/js/*.js", { ignoreInitial: false }, js);
     watch("./scripts/*.js", { ignoreInitial: false }, js);
     watch("./resources/images/*.js", { ignoreInitial: false }, images);
     watch("./resources/favicon.*", { ignoreInitial: false }, images);
 };
 
-exports.default = parallel(html, style, js, images);
+exports.default = series(parallel(html, style, js, images), cleancss);
--- orinoco/src/gulpfile.js/style.js
+++ orinoco/src/gulpfile.js/style.js
@@ -1,9 +1,11 @@
 const { src, dest } = require("gulp");
 const fiber = require("fibers");
 const sass = require("gulp-sass");
+const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const postcss = require("gulp-postcss");
+const purgecss = require("gulp-purgecss");
 
 sass.compiler = require("node-sass");
 
@@ -16,9 +18,21 @@ function style() {
     return src("./scss/**/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass(sassOptions).on("error", sass.logError))
-        .pipe(postcss([autoprefixer()]))
+        .pipe(postcss([tailwindcss(), autoprefixer()]))
         .pipe(sourcemaps.write("."))
+        .pipe(dest(process.env.outputDir + "/debug/css"));
+}
+
+function cleancss() {
+    return src(process.env.outputDir + "/debug/css/*.css")
+        .pipe(
+            purgecss({
+                content: [process.env.outputDir + "/**/*.html", process.env.outputDir + "/**/*.js"],
+            })
+        )
         .pipe(dest(process.env.outputDir + "/css"));
 }
 
 exports.style = style;
+
+exports.cleancss = cleancss;
