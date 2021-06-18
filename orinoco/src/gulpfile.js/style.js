dde9369f3ee050490c7737988b079ffa7816e1f8
benjamin <benjamin.zamour@gmail.com>
2021-06-18 14:27:23 +0200

    tmp commit

--- orinoco/src/gulpfile.js/style.js
+++ orinoco/src/gulpfile.js/style.js
@@ -1,4 +1,4 @@
-const { src, dest } = require("gulp");
+const { src, dest, series } = require("gulp");
 const fiber = require("fibers");
 const sass = require("gulp-sass");
 const tailwindcss = require("tailwindcss");
@@ -18,7 +18,29 @@ function style() {
     return src("./scss/**/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass(sassOptions).on("error", sass.logError))
-        .pipe(postcss([tailwindcss(), autoprefixer()]))
+        .pipe(
+            postcss([
+                tailwindcss({
+                    theme: {
+                        container: {
+                            center: true,
+                            padding: {
+                                DEFAULT: "1rem",
+                                sm: "2rem",
+                                lg: "2rem",
+                                xl: "4rem",
+                                "2xl": "4rem",
+                            },
+                        },
+                        extend: {},
+                        fontFamily: {
+                            logo: ["Pacifico", "Open Sans"],
+                        },
+                    },
+                }),
+                autoprefixer(),
+            ])
+        )
         .pipe(sourcemaps.write("."))
         .pipe(dest(process.env.outputDir + "/debug/css"));
 }
@@ -28,11 +50,20 @@ function cleancss() {
         .pipe(
             purgecss({
                 content: [process.env.outputDir + "/**/*.html", process.env.outputDir + "/**/*.js"],
+                safelist: ["md:text-6xl", "md:h-12", "md:w-12"],
             })
         )
         .pipe(dest(process.env.outputDir + "/css"));
 }
 
+function copyCssMap() {
+    return src(process.env.outputDir + "/debug/css/*.css.map").pipe(
+        dest(process.env.outputDir + "/css")
+    );
+}
+
 exports.style = style;
 
 exports.cleancss = cleancss;
+
+exports.copyCssMap = copyCssMap;
