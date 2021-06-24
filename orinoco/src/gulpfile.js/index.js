b6db2070d1613d5eb97ad9902696c8a70b8ab218
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-06-24 20:10:20 +0200

    add cleancss to gulp tasks

--- orinoco/src/gulpfile.js/index.js
+++ orinoco/src/gulpfile.js/index.js
@@ -4,24 +4,24 @@ process.env.outputDir = process.env.outputDir ?? "./dist";
 
 const { watch, parallel, series } = require("gulp");
 
-const { style, cleancss } = require("./style");
+const { style, cleancss, copyCssMap } = require("./style");
 const { js } = require("./js");
 const { html } = require("./html");
 const { images } = require("./images");
 
 exports.style = style;
-exports.cleancss = cleancss;
+exports.cleancss = series(cleancss, copyCssMap);
 exports.html = html;
 exports.js = js;
 exports.images = images;
 
 exports.watch = function () {
-    watch("./scss/**/*.scss", { ignoreInitial: false }, series(style, cleancss));
-    watch("./views/**/*", { ignoreInitial: false }, series(html, cleancss));
+    watch("./scss/**/*.scss", { ignoreInitial: false }, series(style, exports.cleancss));
+    watch("./views/**/*", { ignoreInitial: false }, series(html, exports.cleancss));
     watch("./resources/js/*.js", { ignoreInitial: false }, js);
     watch("./scripts/*.js", { ignoreInitial: false }, js);
-    watch("./resources/images/*.js", { ignoreInitial: false }, images);
+    watch("./resources/images/*", { ignoreInitial: false }, images);
     watch("./resources/favicon.*", { ignoreInitial: false }, images);
 };
 
-exports.default = series(parallel(html, style, js, images), cleancss);
+exports.default = series(parallel(html, style, js, images), exports.cleancss);
