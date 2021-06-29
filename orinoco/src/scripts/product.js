b18778c0f286bdfbf47d973185aea90163de7732
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-06-29 23:07:28 +0200

    Add documentation + refactoring to product.js

--- orinoco/src/scripts/product.js
+++ orinoco/src/scripts/product.js
@@ -1,3 +1,5 @@
+/** @module js/product */
+
 import { getTeddy } from "./utils.js";
 import TeddyGenerator from "./TeddyGenerator.js";
 import CartStorage from "./CartStorage.js";
@@ -5,14 +7,27 @@ import ToastElement from "./components/ToastElement.js";
 
 const urlParams = new URLSearchParams(window.location.search);
 
+/**
+ * Controller for the product page
+ */
 class Controller {
     #teddy;
     #cart;
+
+    /**
+     * Creat a Controller for the product page
+     * @param {Object} teddy The teddy object presented on the page
+     * @param {CartStorage} cart The cart storage management object
+     */
     constructor(teddy, cart) {
         this.#teddy = teddy;
         this.#cart = cart;
     }
 
+    /**
+     * Initializes the page title with ne teddy's name.
+     * @throws An error if there is no h1 tag on the page.
+     */
     initTitles() {
         document.title = `${this.#teddy.name} l'ourson - Orinoco teddies`;
         let heading = document.getElementsByTagName("h1");
@@ -23,6 +38,11 @@ class Controller {
         heading[0].textContent = this.#teddy.name;
     }
 
+    /**
+     * Initializes the product representation with the teddy data using a TeddyGenerator.
+     *
+     * @see {@link TeddyGenerator}
+     */
     initTeddyElm() {
         const generator = new TeddyGenerator();
 
@@ -34,6 +54,9 @@ class Controller {
         generator.generate({ teddy: this.#teddy, rootElm: elm[0] });
     }
 
+    /**
+     * Binds the events from the page to the Controller's callbacks
+     */
     bind() {
         let btn = document.getElementById("add-to-cart-button");
         btn.addEventListener("click", (event) => {
@@ -41,12 +64,18 @@ class Controller {
         });
     }
 
+    /**
+     * Initializes the controller
+     */
     init() {
         this.initTitles();
         this.initTeddyElm();
         this.bind();
     }
 
+    /**
+     * Add the current product with the quantity and the color selected to the cart
+     */
     addToCart() {
         let count = document.getElementById("number-of-items").input;
         let color = document.getElementById("color-select");
