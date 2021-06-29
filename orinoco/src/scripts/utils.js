5517a96bc73ae90e0ac7ab26e6e5d4f25c2189d0
benjamin <benjamin.zamour@gmail.com>
2021-06-29 09:17:29 +0200

    fixup! Add index scripts

--- orinoco/src/scripts/utils.js
+++ orinoco/src/scripts/utils.js
@@ -15,3 +15,28 @@ export async function getTeddy(id) {
 
     return res.json();
 }
+
+export async function orderTeddy({ firstName, lastName, address, city, email, products }) {
+    let res = await fetch("http://localhost:3000/api/teddies/order", {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            contact: {
+                firstName,
+                lastName,
+                address,
+                city,
+                email,
+            },
+            products,
+        }),
+    });
+    if (!res.ok) {
+        throw new Error(`${res.status}: ${res.statusText}`);
+    }
+
+    return res.json();
+}
