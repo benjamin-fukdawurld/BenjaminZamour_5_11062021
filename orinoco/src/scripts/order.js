26bf55dc272646a31ead9f1a32c33f7cf758b6e6
Benjamin Zamour <benjamin.zamour@gmail.com>
2021-06-29 23:05:21 +0200

    Add documentation + refactoring to order.js

--- orinoco/src/scripts/order.js
+++ orinoco/src/scripts/order.js
@@ -1,72 +1,117 @@
 import { orderTeddy } from "./utils.js";
 import CartStorage from "./CartStorage.js";
 
-const form = document.getElementById("order-form");
-const nextButton = document.getElementById("next");
+/**
+ * Controller for the product page
+ */
+class Controller {
+    #form;
+    #nextButton;
+    #personnalInfo;
+    #payInfo;
+    #confirmation;
+    #steps;
+    #path;
+    #stepIndex;
 
-let personnalInfo = document.getElementById("personnal-info");
-let payInfo = document.getElementById("pay-info");
-let confirmation = document.getElementById("order-confirmation");
+    /**
+     * Creat a Controller for the product page
+     */
+    constructor() {
+        this.#form = document.getElementById("order-form");
+        this.#nextButton = document.getElementById("next");
 
-let steps = document.getElementsByClassName("progress-step__step");
-let path = document.getElementsByClassName("progress-step__path");
-let stepIndex = 0;
+        this.#personnalInfo = document.getElementById("personnal-info");
+        this.#payInfo = document.getElementById("pay-info");
+        this.#confirmation = document.getElementById("order-confirmation");
 
-let fieldsets = form.getElementsByTagName("fieldset");
-
-form.addEventListener("change", () => {
-    nextButton.disabled = !fieldsets[stepIndex].checkValidity();
-});
-
-nextButton.addEventListener("click", (event) => {
-    if (stepIndex === 0) {
-        event.preventDefault();
-        personnalInfo.style.animationPlayState = "running";
-        setTimeout(() => {
-            personnalInfo.style.position = "absolute";
-            personnalInfo.style.zIndex = -1;
-            payInfo.style.animationPlayState = "running";
-            payInfo.style.position = "inherit";
-            payInfo.style.zIndex = 0;
-            steps[1].setAttribute("data-state", "done");
-            path[0].setAttribute("data-state", "done");
-        }, 200);
-
-        ++stepIndex;
-    } else if (stepIndex === 1) {
-        event.preventDefault();
-        form.style.animationPlayState = "running";
-        setTimeout(() => {
-            steps[2].setAttribute("data-state", "done");
-            path[1].setAttribute("data-state", "done");
-            form.style.position = "absolute";
-            form.style.display = "none";
-            confirmation.style.position = "inherit";
-            confirmation.style.display = "flex";
-            confirmation.style.animationPlayState = "running";
-
-            let { firstName, lastName, address, zipcode, city, email } = form;
-            firstName = firstName.value;
-            lastName = lastName.value;
-            address = `${address.value} ${zipcode.value}`;
-            city = city.value;
-            email = email.value;
-
-            let storage = new CartStorage();
-            setTimeout(
-                () =>
-                    orderTeddy({
-                        firstName,
-                        lastName,
-                        address,
-                        city,
-                        email,
-                        products: storage.getProductIds(),
-                    }).then((res) => {
-                        window.location = `confirmation.html?firstName=${res.contact.firstName}&lastName=${res.contact.lastName}&orderId=${res.orderId}`;
-                    }),
-                1000
-            );
-        }, 200);
+        this.#steps = document.getElementsByClassName("progress-step__step");
+        this.#path = document.getElementsByClassName("progress-step__path");
+        this.#stepIndex = 0;
     }
-});
+
+    /**
+     * Send the order to the server
+     */
+    sendOrderToServer() {
+        let { firstName, lastName, address, zipcode, city, email } = this.#form;
+        firstName = firstName.value;
+        lastName = lastName.value;
+        address = `${address.value} ${zipcode.value}`;
+        city = city.value;
+        email = email.value;
+
+        let storage = new CartStorage();
+        setTimeout(
+            () =>
+                orderTeddy({
+                    firstName,
+                    lastName,
+                    address,
+                    city,
+                    email,
+                    products: storage.getProductIds(),
+                }).then((res) => {
+                    window.location = `confirmation.html?firstName=${res.contact.firstName}&lastName=${res.contact.lastName}&orderId=${res.orderId}`;
+                }),
+            1000
+        );
+    }
+
+    /**
+     * Handles the animation from personal information step to credit card info step
+     */
+    handlePersonalInfoStep() {
+        this.#personnalInfo.style.animationPlayState = "running";
+        setTimeout(() => {
+            this.#personnalInfo.style.position = "absolute";
+            this.#personnalInfo.style.zIndex = -1;
+            this.#payInfo.style.animationPlayState = "running";
+            this.#payInfo.style.position = "inherit";
+            this.#payInfo.style.zIndex = 0;
+            this.#steps[1].setAttribute("data-state", "done");
+            this.#path[0].setAttribute("data-state", "done");
+        }, 200);
+
+        ++this.#stepIndex;
+    }
+
+    /**
+     * Handles the animation from credit card step to confirmation step
+     */
+    handleCreditCardInfoStep() {
+        this.#form.style.animationPlayState = "running";
+        setTimeout(() => {
+            this.#steps[2].setAttribute("data-state", "done");
+            this.#path[1].setAttribute("data-state", "done");
+            this.#form.style.position = "absolute";
+            this.#form.style.display = "none";
+            this.#confirmation.style.position = "inherit";
+            this.#confirmation.style.display = "flex";
+            this.#confirmation.style.animationPlayState = "running";
+        }, 200);
+
+        this.sendOrderToServer();
+    }
+
+    /**
+     * Initializes the controller
+     */
+    init() {
+        this.#form.addEventListener("change", () => {
+            this.#nextButton.disabled = !this.#form.checkValidity();
+        });
+
+        this.#nextButton.addEventListener("click", (event) => {
+            event.preventDefault();
+            if (this.#stepIndex === 0) {
+                this.handlePersonalInfoStep();
+            } else if (this.#stepIndex === 1) {
+                this.handleCreditCardInfoStep();
+            }
+        });
+    }
+}
+
+let controller = new Controller();
+controller.init();
